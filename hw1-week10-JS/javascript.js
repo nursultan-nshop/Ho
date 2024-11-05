9870515310d15d8666b509fa280648e6ab028f1c
Nurs <abylajhanulyn@gmail.com>
2024-11-05 15:51:27 +0500

    Homework

---
+++ hw1-week10-JS/javascript.js
@@ -0,0 +1,14 @@
+let = 60
+let isStudent = true
+
+alert((age>=65 || isStudent))
+
+
+let num = Number(prompt('Write a number:'))
+alert(num>=10 && num<=20)
+
+
+let num1 = Number(prompt('Write a number1:'))
+let num2 = Number(prompt('Write a number2:'))
+
+alert((num1<=0 && num2>=0) || (num1>=0 && num2<=0))
