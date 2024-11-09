6b4aa164c8619339fd79fbf8a4bcb3e7f0915db4
Nurs <abylajhanulyn@gmail.com>
2024-11-09 08:57:09 +0500

    HomeWork

---
+++ hw4-week10-JS/javascript.js
@@ -0,0 +1,20 @@
+let num1 = +prompt('Birinshi sandy engiziniz:')
+let num2 = +prompt('Ekinshi  sandy engiziniz:')
+let num3 = +prompt('Ushinshi sandy engiziniz:')
+
+num1>=num2 == num1
+num2>=num3 == num2
+num3>=num1 == num3
+
+switch(num1){
+    case num1:
+        alert(' En ulken san:' + num1)
+    break;
+    case num2:
+        alert('En ulken san:' + num2)
+    break;
+    case num3:
+        alert('En ulken san:' + num3)
+    break;
+    default:'Error'
+}
