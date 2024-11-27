1242dc61a451e4106c9a38dcb4b008bfbdb4b240
Nurs <abylajhanulyn@gmail.com>
2024-11-27 15:50:16 +0500

    HomeWork

---
+++ hw3-week13-JS/script.js
@@ -0,0 +1,15 @@
+
+let footballistter = ['Ronaldo', 'Zidan', 'Neymar', 'Benzema']
+
+footballistter[1] = 'Messi'
+footballistter[4] = 'Ramos'
+footballistter[5] = 'Iniesta'
+footballistter[6] = 'Ibrahimovich'
+
+if (footballistter > 5) {
+  console.log('Kop')
+} else {
+  console.log('Az')
+}
+
+console.log(footballistter)
